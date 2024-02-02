ffcd594ee2f2cbfcaaead60ecde26ad7616b3baa
Lewis Sobotkiewicz <lewis.sobot@gmail.com>
2024-02-02 00:50:05 +0000

    Deleting some old Javascript code

--- spec/local.spec.js
+++
@@ -1,8 +0,0 @@
-describe("The local-process-memory lock provider", function () {
-	var providerConfig = {
-		provider: 'local'
-	};
-
-	// Run full test suite with this provider.
-	require('./provider-agnostic').getTests(providerConfig);
-});
